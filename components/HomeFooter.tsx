5eb1935c563aa5528c2a86e96244364b8558838a
zsofiaordog <zsofia.ordog@gmail.com>
2026-07-20 14:57:36 +0200

    Update Homefooter

--- components/HomeFooter.tsx
+++ components/HomeFooter.tsx
@@ -1,6 +1,6 @@
 export default function HomeFooter() {
   return (
-    <footer className="absolute bottom-10 left-0 z-10 w-full text-center text-xs uppercase tracking-[0.3em] text-white/50">
+    <footer className="absolute bottom-10 left-0 z-10 w-full text-center text-xs tracking-[0.3em] text-white/50">
       © {new Date().getFullYear()} Zsófia ördög. All Rights Reserved
     </footer>
   );
