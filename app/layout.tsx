8dc987e1f5eb1a0108f1719449c7026eae7ca56e
zsofiaordog <zsofia.ordog@gmail.com>
2026-07-23 22:40:09 +0200

    Update changes in layout nad global

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,65 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const avenirNext = localFont({
+  src: [
+    {
+      path: "../public/fonts/Avenir Next Ultra Light.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Avenir Next Thin.woff2",
+      weight: "250",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Avenir Next Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Avenir Next Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Avenir Next Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Avenir Next Demi.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Avenir Next Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Avenir Next Heavy.woff2",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  preload: true,
+  variable: "--font-avenir-next",
+  fallback: ["Arial", "Helvetica", "sans-serif"],
+});
 
 export const metadata: Metadata = {
   title: "Zsófia Ördög | Film Editor",
-  description:
-    "Portfolio of Zsófia Ördög",
+  description: "Portfolio of Zsófia Ördög",
   icons: {
     icon: "/favicon.ico",
   },
   openGraph: {
     title: "Zsófia Ördög | Film Editor",
-    description:
-      "Portfolio of Zsófia Ördög",
+    description: "Portfolio of Zsófia Ördög",
     siteName: "Zsófia Ördög",
   },
 };
@@ -22,8 +70,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html
+      lang="en"
+      className={`${avenirNext.variable} ${avenirNext.className} h-full antialiased`}
+    >
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
