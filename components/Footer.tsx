c9a7bc9809f814053891d89a8ab13edc7d3d1f9a
zsofiaordog <zsofia.ordog@gmail.com>
2026-08-09 21:28:37 +0200

    update footer and homefooter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full mt-32 py-12 text-center text-xs tracking-[0.12em] opacity-50">
-      © {new Date().getFullYear()} Zsófia Ördög. All Rights Reserved
+      © {new Date().getFullYear()} All Rights Reserved
     </footer>
   );
 }
